bce3aef1f2df838a093272dbc81682cfd5f3be98
Jinal Parmar <84787309+jinal77777@users.noreply.github.com>
2021-06-01 23:58:22 +0530

    Add files via upload

---
+++ Day-3/get.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+ 
+   <form method='post' action='process.php'>
+   <table border=1px black border=collapse : collapse>
+   <h1>registration form</h1>
+        <tr>
+  	   <td>first name</td>
+           <td><input type="text" name="txt1" placeholder='enter name'required/></td>
+        </tr>
+        <tr>
+           <td>last name:</td>
+           <td><input type="text" name="txt2"placeholder='enter lastname'                       required/></td>
+        </tr>
+        <tr>
+           <td>email:</td>
+           <td><input type="text" name="txt3"placeholder='enter email'                           required/></td>
+        </tr>
+        <tr> 
+            <td>date of birth:</td>
+           <td><input type="date" name="txt4"placeholder='enter date of birth'                   required/></td>
+        </tr> 
+        <tr>
+            <td><input type="submit" /></td>
+            <td><input type="reset" /></td>
+         </tr>
+       </table>
+    </form>
+ </body>
+</html>
