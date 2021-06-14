39a30c070fb7723ddf46366d2846c371b057dfc9
JINAL PARMAR <84787309+jinal77777@users.noreply.github.com>
2021-06-14 17:32:51 +0530

    Add files via upload

---
+++ Day-4/footer.php
@@ -0,0 +1,4 @@
+</div>
+<div id="footer">
+	<p>2021.Website Managed by Anjali. </p>
+</div>
---
+++ Day-4/sidebar.php
@@ -0,0 +1,8 @@
+<div id="sidebar">
+    <h2>News</h2>
+    
+    <marquee>
+        Welcome to Websit.
+    </marquee>
+		</div>
+		<!-- end #sidebar -->
